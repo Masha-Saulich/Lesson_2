dea57028ea9cc9920caf88bdd4dedf2d88559725
Masha-Saulich <mariya.saulich@gmail.com>
2024-10-28 20:30:40 +0300

    add Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,20 @@ package org.example;
 public class Main {
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
         try {
-            Size();
+            String[][] s1 = {
+                    {"1", "2", "3","4"},
+                    {"5", "6", "7", "8"},  // Создан один массив для двух методов.
+                                            // Чтобы выдать исключение, нужно внести изменения.
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "15", "16"}
+            };
+            checkSize(s1);
+            convertSize(s1);
         } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
     }
-
-    public static void Size() throws MyArraySizeException, MyArrayDataException {
-        String[][] s1 = {
-                {"1", "2", "3", "4"},
-                {"5", "6", "8"},
-                {"9", "q", "11", "12"},
-                {"13", "14", "15", "16"}};
-
+    public static void checkSize(String[][] s1) throws MyArraySizeException{
         if (s1.length != 4) {
             throw new MyArraySizeException("Массив должен содержать 4 строки");
         }
@@ -24,17 +25,14 @@ public class Main {
                 throw new MyArraySizeException("Строка " + (i + 1) + " должна содержать 4 элемента.");
             }
         }
+    } public static void convertSize(String[][]s1) throws MyArrayDataException{
 
         int[][] intArray = new int[s1.length][s1[0].length];
 
         for (int i = 0; i < s1.length; i++) {
             for (int j = 0; j < s1[i].length; j++) {
-                try {
-                    intArray[i][j] = Integer.parseInt(s1[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Ошибка преобразования в строке " + i + ", столбец " + j + ": " + s1[i][j]);
+                throw new MyArrayDataException("Ошибка преобразования в строке " + i + ", столбец " + j + ": " + s1[i][j]);
                 }
             }
         }
     }
-}
