9c5e1204480e3739a6778ac48c76aa84713217bf
Masha-Saulich <mariya.saulich@gmail.com>
2024-11-12 21:56:52 +0300

    Домашка 16 вся

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -5,17 +6,22 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+
+import static java.awt.SystemColor.text;
 
 
 public class HomePage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     public void acceptCookie() {
-        WebElement cookieButton = driver.findElement(By.id("cookie-accept"));
+        WebElement cookieButton = driver.findElement(By.id("cookie-agree"));
         cookieButton.click();
     }
 
@@ -58,6 +64,46 @@ public class HomePage {
         option.click();
 
     }
+
+    public void switchToIframe() {
+        WebElement iframe = driver.findElement(By.className("bepaid-iframe"));
+        driver.switchTo().frame(iframe);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("app-wrapper__content")));
+    }
+
+    public static String cleanText(WebElement element, String... toRemove) {
+        String text = element.getText();
+        for (String removeText : toRemove) {
+            text = text.replace(removeText, "");
+        }
+        return text.trim();
+    }
+
+    public void getLabelName(String labelText) {
+        switchToIframe();
+        WebElement label = driver.findElement(By.xpath("//label[text()='" + labelText + "']"));
+        String actualText = label.getText().trim();
+        Assertions.assertEquals(actualText, labelText);
+    }
+    public void getLogoCard (String logoName) {
+        switchToIframe();
+        List<WebElement> logoList = driver.findElements(By.xpath("//img[contains(@src, '" + logoName + "')]"));
+        // Проверка, что список логотип не пуст
+        Assertions.assertFalse(logoList.isEmpty(), "Логотип " + logoName + " не найден на странице!");
+    }
+    public void checkPaymentSum(String elementLocator, String...toRemove) {
+        switchToIframe();
+        WebElement sum = driver.findElement(By.xpath(elementLocator));
+        String actualText = HomePage.cleanText(sum, toRemove);
+        double actualSum = Double.parseDouble(actualText);
+        double expectedSum = 13.00;
+        Assertions.assertEquals(actualSum, expectedSum, "Ожидаемая сумма платежа не совпадает с реальной!");
+    }
+    public void checkPhone(String elementLocator, String...toRemove) {
+        switchToIframe();
+        WebElement phone = driver.findElement(By.xpath(elementLocator));
+        String actualText = HomePage.cleanText(phone, toRemove);
+        String expectedText = "375297777777";
+        Assertions.assertEquals(actualText, expectedText, "Ожидаемый номер телефона не совпадает с реальным!");
+    }
 }
-
-
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -9,22 +9,23 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
 import java.time.Duration;
+import java.util.List;
 
 public class TestClass {
     private WebDriver driver;
     private HomePage HomePage;
+    private WebDriverWait wait;
 
     @BeforeEach
     public void initDriver() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\.cache\\selenium\\chromedriver\\win64\\130.0.6723.116\\chromedriver.exe");
         driver = new ChromeDriver();
-        HomePage  = new HomePage(driver);
+        HomePage = new HomePage(driver);
         driver.get("https://www.mts.by/");
         driver.manage().deleteAllCookies();
         driver.navigate().refresh();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        WebElement input = driver.findElement(By.id("cookie-agree"));
-        input.click();
+        HomePage.acceptCookie();
 
     }
 
@@ -39,171 +40,270 @@ public class TestClass {
     @Test
     @DisplayName("Проверка логотипа Visa")
     public void test() {
-            String actualText = HomePage.getLogoText("Visa");
-            Assertions.assertEquals("Visa", actualText);
+        String actualText = HomePage.getLogoText("Visa");
+        Assertions.assertEquals("Visa", actualText);
     }
 
     @Test
     @DisplayName("Проверка логотипа Verified By Visa")
-    public void test2 () {
+    public void test2() {
         String actualText = HomePage.getLogoText("Verified By Visa");
         Assertions.assertEquals("Verified By Visa", actualText);
-                }
+    }
+
     @Test
     @DisplayName("Проверка логотипа MasterCard")
-    public void test3 () {
+    public void test3() {
         String actualText = HomePage.getLogoText("MasterCard");
         Assertions.assertEquals("MasterCard", actualText);
     }
 
     @Test
     @DisplayName("Проверка логотипа MasterCard Secure Code")
-    public void test4 () {
+    public void test4() {
         String actualText = HomePage.getLogoText("MasterCard Secure Code");
         Assertions.assertEquals("MasterCard Secure Code", actualText);
     }
 
     @Test
     @DisplayName("Проверка логотипа Белкарт")
-    public void test5 () {
+    public void test5() {
         String actualText = HomePage.getLogoText("Белкарт");
         Assertions.assertEquals("Белкарт", actualText);
     }
 
     @Test
     @DisplayName("проверка ссылки Подробнее о сервисе")
-    public void test6 () {
+    public void test6() {
         HomePage.clickLink();
 
     }
+
     @Test
     @DisplayName("Заполнение полей")
-    public void test7 () {
+    public void test7() {
         HomePage.fillForm("297777777", "13", "test@example.com");
         HomePage.submitForm();
 
     }
+
     @Test
     @DisplayName("Услуги связи: проверка плейсхолдера поля Номер телефона")
-    public void test8 () {
+    public void test8() {
         String actualText = HomePage.getPlaceholderByID("connection-phone");
         String expectedText = "Номер телефона";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Услуги связи: проверка плейсхолдера поля Сумма")
-    public void test9 () {
+    public void test9() {
         String actualText = HomePage.getPlaceholderByID("connection-sum");
         String expectedText = "Сумма";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Услуги связи: проверка плейсхолдера поля email")
-    public void test10 () {
+    public void test10() {
         String actualText = HomePage.getPlaceholderByID("connection-email");
         String expectedText = "E-mail для отправки чека";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Выбрать опцию: Домашний интерет")
-    public void test11 () {
+    public void test11() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Домашний интернет");
 
     }
+
     @Test
     @DisplayName("Домашний интернет: проверка плейсхолдера поля Номер абонента")
-    public void test12 () {
+    public void test12() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Домашний интернет");
         String actualText = HomePage.getPlaceholderByID("internet-phone");
         String expectedText = "Номер абонента";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Домашний интернет: проверка плейсхолдера поля Сумма")
-    public void test13 () {
+    public void test13() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Домашний интернет");
         String actualText = HomePage.getPlaceholderByID("internet-sum");
         String expectedText = "Сумма";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Домашний интернет: проверка плейсхолдера поля email")
-    public void test14 () {
+    public void test14() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Домашний интернет");
         String actualText = HomePage.getPlaceholderByID("internet-email");
         String expectedText = "E-mail для отправки чека";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Выбрать опцию: Рассрочка")
-    public void test15 () {
+    public void test15() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");
 
     }
+
     @Test
     @DisplayName("Рассрочка: проверка плейсхолдера поля Номер счета на 44")
-    public void test16 () {
-        HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");;
+    public void test16() {
+        HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");
         String actualText = HomePage.getPlaceholderByID("score-instalment");
         String expectedText = "Номер счета на 44";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Рассрочка: проверка плейсхолдера поля Сумма")
-    public void test17 () {
-        HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");;
+    public void test17() {
+        HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");
         String actualText = HomePage.getPlaceholderByID("instalment-sum");
         String expectedText = "Сумма";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Рассрочка: проверка плейсхолдера поля E-mail для отправки чека")
-    public void test18 () {
-        HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");;
+    public void test18() {
+        HomePage.selectOptionFromDropdown("select__wrapper", "Рассрочка");
+        ;
         String actualText = HomePage.getPlaceholderByID("instalment-email");
         String expectedText = "E-mail для отправки чека";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Выбрать опцию: Задолженность")
     public void test19() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Задолженность");
 
     }
+
     @Test
     @DisplayName("Задолженность: проверка плейсхолдера поля Номер счета на 2073")
-    public void test20 () {
+    public void test20() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Задолженность");
         String actualText = HomePage.getPlaceholderByID("score-arrears");
         String expectedText = "Номер счета на 2073";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Задолженность: проверка плейсхолдера поля Сумма")
-    public void test21 () {
+    public void test21() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Задолженность");
         String actualText = HomePage.getPlaceholderByID("arrears-sum");
         String expectedText = "Сумма";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
     @DisplayName("Задолженность: проверка плейсхолдера поля E-mail для отправки чека")
-    public void test22 () {
+    public void test22() {
         HomePage.selectOptionFromDropdown("select__wrapper", "Задолженность");
         String actualText = HomePage.getPlaceholderByID("arrears-email");
         String expectedText = "E-mail для отправки чека";
         Assertions.assertEquals(expectedText, actualText);
     }
+
     @Test
-    @DisplayName("Услуги связи: заполнить поля и продолжить")
-    public void test23 () {
+    @DisplayName("Проверка корректности отображения суммы")
+    public void test23() {
         HomePage.fillForm("297777777", "13", "test@example.com");
         HomePage.submitForm();
+        HomePage.checkPaymentSum("//div[@class='pay-description__cost']", "BYN");
+    }
+    @Test
+    @DisplayName("Проверка суммы на кнопке")
+    public void test24() {
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.checkPaymentSum("//button[@type='submit']","Оплатить", "BYN");
+    }
+    @Test
+    @DisplayName("Проверка отображения номера телефона")
+    public void test25() {
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.checkPhone("//div[@class='pay-description__text']", "Оплата: Услуги связи Номер:");
 
     }
+    @Test
+    @DisplayName("Проверка наличия надписи Номер карты")
+    public void test26(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLabelName("Номер карты");
 
-        @AfterEach
-        public void closeDriver () {
-            driver.quit();
-        }
     }
+    @Test
+    @DisplayName("Проверка наличия надписи Срок действия")
+    public void test27(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLabelName("Срок действия");
+    }
+    @Test
+    @DisplayName("Проверка наличия надписи CVC")
+    public void test28(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLabelName("CVC");
+    }
+    @Test
+    @DisplayName("Проверка наличия надписи Имя держателя(как на карте)")
+    public void test29(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLabelName("Имя держателя (как на карте)");
+    }
+    @Test
+    @DisplayName("Проверка наличия логотипа Visa")
+    public void test30(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLogoCard("visa-system.svg");
+
+    }
+    @Test
+    @DisplayName("Проверка наличия логотипа MasterCard")
+    public void test31(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLogoCard("mastercard-system.svg");
+    }
+    @Test
+    @DisplayName("Проверка наличия логотипа Белкарт")
+    public void test32(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLogoCard("belkart-system.svg");
+    }
+    @Test
+    @DisplayName("Проверка наличия логотипа Maestro")
+    public void test33(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLogoCard("maestro-system.svg");
+    }
+    @Test
+    @DisplayName("Проверка наличия логотипа Мир")
+    public void test34(){
+        HomePage.fillForm("297777777", "13", "test@example.com");
+        HomePage.submitForm();
+        HomePage.getLogoCard("mir-system-ru.svg");
+    }
+
+    @AfterEach
+    public void closeDriver() {
+        driver.quit();
+    }
+}
