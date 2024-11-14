a32e146305998cfd595179f6e58d86388d7c0967
Masha-Saulich <mariya.saulich@gmail.com>
2024-11-14 17:31:22 +0300

    +

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,4 +1,6 @@
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
@@ -11,21 +13,25 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 import java.awt.*;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 
 import static com.google.common.base.Predicates.equalTo;
-import static io.restassured.RestAssured.*;
 
-public class TestClass {
+public class ApiTest {
 
     @Test
     @DisplayName("GET Request")
     public void test() {
         given()
                 .baseUri("https://postman-echo.com/")
+                .contentType(ContentType.JSON)
                 .when()
                 .get("get")
                 .then()
@@ -39,7 +45,7 @@ public class TestClass {
     public void test1() {
         given()
                 .baseUri("https://postman-echo.com/")
-                .contentType(ContentType.TEXT)
+                .contentType(ContentType.JSON)
                 .when().body("{\n" +
                         "    \"test\": \"value\"\n" +
                         "}")
@@ -50,30 +56,32 @@ public class TestClass {
 
     @Test
     @DisplayName("POST Form Data")
-    public void test2() {
-        HashMap <String, String> form = new HashMap<>();
+            public void test2 () {
+        HashMap<String, String> form = new HashMap<>();
         form.put("foo1", "bar1");
         form.put("foo2", "bar2");
 
-        given()
-                .baseUri("https://postman-echo.com/post")
-                .contentType("application/x-www-form-urlencoded")
-                .formParams(form)
-                .when()
-                .post()
-                .then().assertThat()
-                .statusCode(200)
-                .log().body()
-                .body("form.foo1", Matchers.equalTo("bar1"))
-                .body("form.foo2", Matchers.equalTo("bar2"));
 
-}
+                given()
+                        .baseUri("https://postman-echo.com")
+                        .contentType("application/json")
+                        .body(form)
+                        .when()
+                        .post("/post")
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .body("data.foo1", Matchers.equalTo("bar1"))
+                        .body("data.foo2", Matchers.equalTo("bar2"))
+                        .log().body();
+
+            }
     @Test
     @DisplayName("PUT Request")
     public void test3() {
         given()
                 .baseUri("https://postman-echo.com/")
-               .contentType(ContentType.TEXT)
+               .contentType(ContentType.JSON)
                .when()
                 .body("This is expected to be sent back as part of response body.")
                .put("put")
@@ -86,7 +94,7 @@ public class TestClass {
     public void test4() {
         given()
                 .baseUri("https://postman-echo.com/")
-                .contentType(ContentType.TEXT)
+                .contentType(ContentType.JSON)
                 .when()
                 .body("This is expected to be sent back as part of response body.")
                 .patch("patch")
@@ -99,11 +107,13 @@ public class TestClass {
     public void test5() {
         given()
                .baseUri("https://postman-echo.com/")
+                .contentType(ContentType.JSON)
                .when()
                 .body("This is expected to be sent back as part of response body.")
                .delete("delete")
                .then().assertThat()
                .statusCode(200)
                .log().body();
+
     }
-    }
+          }
